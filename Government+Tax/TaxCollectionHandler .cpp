13ccc16218cb5704ee7164042853b943d60a7e15
MilanThenerd <50702929+MilanThenerd@users.noreply.github.com>
2024-10-25 19:30:41 +0200

    Delete Government+Tax/TaxCollectionHandler .cpp

--- Government+Tax/TaxCollectionHandler .cpp
+++
@@ -1,6 +0,0 @@
-#include "TaxCollectionHandler .h"
-
-void TaxCollectionHandler_::handleRequest(TaxCommand* command) {
-	// TODO - implement TaxCollectionHandler ::handleRequest
-	throw "Not yet implemented";
-}
