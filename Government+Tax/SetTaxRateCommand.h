d9354bca60c41c0b879cd44dabe99443c5e2105d
MilanThenerd <50702929+MilanThenerd@users.noreply.github.com>
2024-10-26 19:39:44 +0200

    Update SetTaxRateCommand.h

--- Government+Tax/SetTaxRateCommand.h
+++ Government+Tax/SetTaxRateCommand.h
@@ -6,12 +6,10 @@
 #include <string>
 
 class SetTaxRateCommand : public TaxCommand {
-
 private:
 	Government* government;
 	double taxRate;
 	std::string category;
-
 public:
 	SetTaxRateCommand(Government* gov, double rate, std::string cat);
 	void execute();
