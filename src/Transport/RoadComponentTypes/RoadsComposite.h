d08d40a82d506c52f1ae1e63a9622226ace3d012
u04576617 <u04576617@tuks.co.za>
2024-10-31 16:28:36 +0200

    Add some descriptions of functions

--- src/Transport/RoadComponentTypes/RoadsComposite.h
+++ src/Transport/RoadComponentTypes/RoadsComposite.h
@@ -10,6 +10,14 @@ private:
 	std::vector<RoadComponent *> components;
 
 public:
+	/*
+	 * @brief Constructor for RoadsComposite
+	 * @param sX - start x coordinate
+	 * @param sY - start y coordinate
+	 * @param eX - end x coordinate
+	 * @param eY - end y coordinate
+	 * @param type - type of road (highway, main, residential)
+	 */
 	RoadsComposite(int sX, int sY, int eX, int eY, std::string type);
 
 	void displayInfo();
@@ -25,6 +33,11 @@ public:
 	void notifyChange(std::string message);
 
 	void addConnection(RoadComponent *connection, float distance);
+
+	const std::vector<RoadComponent *> &getComponents() const
+	{
+		return components;
+	}
 
 	~RoadsComposite() {}
 };
--- tests/basic_transportation.cpp
+++ tests/basic_transportation.cpp
@@ -1,6 +1,20 @@
 #include "../src/Transport/TransportInclude.h"
+#include <gtest/gtest.h>
 
-void testConstructors()
+TEST(CompositeTest, CheckCompositeLength)
+{
+    RoadsComposite *roadsComposite = new RoadsComposite(0, 0, 100, 100, "highway");
+    roadsComposite->displayInfo();
+
+    float val = 141.421356237;
+    bool roadsCompositeInRange = abs(roadsComposite->getDistance() - val) < 0.0001;
+    EXPECT_TRUE(roadsCompositeInRange);
+
+    std::vector<RoadComponent *> components = roadsComposite->getComponents();
+    EXPECT_EQ(components.size(), 3);
+}
+
+TEST(ConstructorTest, CheckDistance)
 {
     Highways *highway = new Highways(0, 0, 10, 10);
     MainRoads *mainRoad = new MainRoads(0, 0, 10, 10);
@@ -11,10 +25,22 @@ void testConstructors()
     mainRoad->displayInfo();
     residentialStreet->displayInfo();
     roadsComposite->displayInfo();
+
+    float val = 14.1421356237;
+
+    bool highwayInRange = abs(highway->getDistance() - val) < 0.0001;
+    bool mainRoadInRange = abs(mainRoad->getDistance() - val) < 0.0001;
+    bool residentialStreetInRange = abs(residentialStreet->getDistance() - val) < 0.0001;
+    bool roadsCompositeInRange = abs(roadsComposite->getDistance() - val) < 0.0001;
+
+    EXPECT_TRUE(highwayInRange);
+    EXPECT_TRUE(mainRoadInRange);
+    EXPECT_TRUE(residentialStreetInRange);
+    EXPECT_TRUE(roadsCompositeInRange);
 }
 
 int main()
 {
-    testConstructors();
-    return 0;
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
