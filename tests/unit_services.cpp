05c73228a825c13f27c2fe38c1e62855a71f4ff1
AAW <147758079+Amber21457752@users.noreply.github.com>
2024-11-02 17:36:18 +0200

    Added tests to Services

--- tests/unit_services.cpp
+++ tests/unit_services.cpp
@@ -89,6 +89,57 @@ TEST_F(ServEducationTest, AddAndRemoveEmployee) {
     delete employee;
 }
 
+// Test fixture for ServSecurity
+class ServSecurityTest : public ::testing::Test {
+protected:
+    ServSecurity* securityService;
+
+    void SetUp() override {
+        securityService = new ServSecurity();
+    }
+
+    void TearDown() override {
+        delete securityService;
+    }
+};
+
+// Test security service creation
+TEST_F(ServSecurityTest, CreateServSecurity) {
+    ASSERT_NE(securityService, nullptr);
+}
+
+// Test setting and getting visitors
+TEST_F(ServSecurityTest, SetAndGetVisitors) {
+    securityService->setVisitors(25);
+    EXPECT_EQ(securityService->getVisitors(), 25);
+    
+    securityService->setVisitors(10);
+    EXPECT_EQ(securityService->getVisitors(), 10);
+}
+
+// Test adding and removing employees
+TEST_F(ServSecurityTest, AddAndRemoveEmployee) {
+    Citizen* employee = new Citizen();
+    EXPECT_TRUE(securityService->addEmployee(employee));
+    securityService->removeEmployee(employee);
+    delete employee;
+}
+
+// Test state setting and getting
+TEST_F(ServSecurityTest, SetAndGetState) {
+    securityService->setState(true);
+    EXPECT_TRUE(securityService->getState());
+    
+    securityService->setState(false);
+    EXPECT_FALSE(securityService->getState());
+}
+
+// Test calling utilities
+TEST_F(ServSecurityTest, CallUtilities) {
+    // Assuming notifyUtilities is a method to check if utilities are called
+    EXPECT_NO_THROW(securityService->callUtilities());
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
