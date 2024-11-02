aece7c993f6359637d8841c7e7a9c3dec15b92d4
AAW <147758079+Amber21457752@users.noreply.github.com>
2024-11-02 17:18:00 +0200

    Added Tests to Landmarks

--- tests/unit_landmark.cpp
+++ tests/unit_landmark.cpp
@@ -8,6 +8,7 @@
 #include "../src/Citizens/Citizen.h"
 
 #include "../src/Utilities/UtilPowerPlants.h"
+#include "../src/Utilities/UtilWaterSupply.h"
 
 // Test fixture for Landmark
 class LandmarkTest : public ::testing::Test {
@@ -93,6 +94,59 @@ TEST_F(LandCCenterTest, DisplayBuildingInfo) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_NE(output.find("Community center with"), std::string::npos); // Check that output contains expected string
 }
+
+// Test fixture for LandPark
+class LandParkTest : public ::testing::Test {
+protected:
+    LandPark* park;
+
+    void SetUp() override {
+        park = new LandPark();
+    }
+
+    void TearDown() override {
+        delete park;
+    }
+};
+
+// Test LandPark creation
+TEST_F(LandParkTest, CreateLandPark) {
+    ASSERT_NE(park, nullptr);
+}
+
+// Test setting and getting visitors
+TEST_F(LandParkTest, SetAndGetVisitors) {
+    park->setVisitors(20);
+    EXPECT_EQ(park->getVisitors(), 20);
+}
+
+// Test utility management for LandPark
+TEST_F(LandParkTest, AddAndRemoveUtilityLandPark) {
+    UtilityManager* utility = new UtilWaterSupply();
+    park->addUtility(utility);
+    EXPECT_NO_THROW(park->notifyUtilities());
+    park->removeUtility(utility);
+    EXPECT_NO_THROW(park->notifyUtilities());
+    delete utility;
+}
+
+// Test setting and getting operational state for LandPark
+TEST_F(LandParkTest, SetAndGetStateLandPark) {
+    park->setState(true);
+    EXPECT_TRUE(park->getState());
+    park->setState(false);
+    EXPECT_FALSE(park->getState());
+}
+
+// Test LandPark info display
+TEST_F(LandParkTest, DisplayBuildingInfoLandPark) {
+    park->setVisitors(8);
+    testing::internal::CaptureStdout();
+    park->displayBuildingInfo();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Park with 8 visitors"), std::string::npos);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
