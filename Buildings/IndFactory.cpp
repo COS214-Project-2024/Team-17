e4cd1345d92091d69b08fb46111f3d0ca7cf39a5
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:44:39 +0200

    Update IndFactory.cpp

--- Buildings/IndFactory.cpp
+++ Buildings/IndFactory.cpp
@@ -1,17 +1,45 @@
 #include "IndFactory.h"
 
+/**
+ * @class IndFactory
+ * @brief Represents an industrial factory building.
+ * 
+ * This class provides methods to manage and display information about 
+ * an industrial factory, including its production capacity.
+ */
+
+/**
+ * @brief Constructor for the IndFactory class.
+ * 
+ * Initializes a new instance of the IndFactory and outputs a creation message.
+ */
 IndFactory::IndFactory() {
     cout << BLACK << "\t-->Factory created" << RESET << endl;
 }
 
+/**
+ * @brief Displays information about the factory.
+ * 
+ * This method outputs the production capacity of the factory to the console.
+ */
 void IndFactory::displayBuildingInfo() {
     cout << "Factory with production capacity of " << this->productionCapacity << " units\n";
 }
 
+/**
+ * @brief Gets the production capacity of the factory.
+ * 
+ * @return The current production capacity of the factory.
+ */
 int IndFactory::getProductionCapacity() {
     return productionCapacity;
 }
 
+/**
+ * @brief Sets the production capacity of the factory.
+ * 
+ * @param capacity The new production capacity to set for the factory.
+ */
 void IndFactory::setProductionCapacity(int capacity) {
     this->productionCapacity = capacity;
 }
