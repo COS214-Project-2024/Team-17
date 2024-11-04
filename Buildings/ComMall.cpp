8aacb6014091e30c3ee82cee7137e3610632aa03
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:33:16 +0200

    Update ComMall.cpp

--- Buildings/ComMall.cpp
+++ Buildings/ComMall.cpp
@@ -1,17 +1,45 @@
 #include "ComMall.h"
 
+/**
+ * @class ComMall
+ * @brief Represents a commercial mall building.
+ * 
+ * This class defines a mall with a specific job capacity and methods to 
+ * display information about the mall and manage its job capacity.
+ */
+
+/**
+ * @brief Constructor for the ComMall class.
+ * 
+ * Initializes a new instance of the ComMall and outputs a creation message.
+ */
 ComMall::ComMall() {
     cout << BLACK << "\t-->Mall created" << RESET << endl;
 }
 
+/**
+ * @brief Displays information about the mall.
+ * 
+ * Outputs the job capacity of the mall.
+ */
 void ComMall::displayBuildingInfo() {
     cout << "Mall with " << this->jobCapacity << " jobs\n";
 }
 
+/**
+ * @brief Gets the job capacity of the mall.
+ * 
+ * @return The current job capacity of the mall.
+ */
 int ComMall::getJobCapacity() {
     return jobCapacity;
 }
 
+/**
+ * @brief Sets the job capacity of the mall.
+ * 
+ * @param capacity The new job capacity to be set for the mall.
+ */
 void ComMall::setJobCapacity(int capacity) {
     this->jobCapacity = capacity;
 }
