b7c93f9e69916a4221dedfd46ac9923a48bf5546
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:40:01 +0200

    Update FactLandmarks.cpp

--- Buildings/FactLandmarks.cpp
+++ Buildings/FactLandmarks.cpp
@@ -1,9 +1,29 @@
 #include "FactLandmarks.h"
 
+/**
+ * @class FactLandmarks
+ * @brief Factory class for creating landmark instances.
+ * 
+ * This class implements a factory pattern to create instances of 
+ * various landmark types such as parks, community centers, and monuments.
+ */
+
+/**
+ * @brief Constructor for the FactLandmarks class.
+ * 
+ * Initializes a new instance of the FactLandmarks and outputs a creation message.
+ */
 FactLandmarks::FactLandmarks() {
     cout << BLACK << "\t->Landmark factory created" << RESET << endl;
 }
 
+/**
+ * @brief Creates a landmark based on the specified type.
+ * 
+ * @param type A string representing the type of landmark to create.
+ * 
+ * @return A pointer to the created Landmark object, or nullptr if the type is not recognized.
+ */
 Landmark* FactLandmarks::createLandmark(string type) {
     if (type == "Park") {
         return new LandPark();
@@ -15,16 +35,54 @@ Landmark* FactLandmarks::createLandmark(string type) {
     return nullptr;
 }
 
+/**
+ * @brief Creates a residential building based on the specified type.
+ * 
+ * This method is not implemented for landmark factories.
+ * 
+ * @param type A string representing the type of residential building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Residential* FactLandmarks::createResBuilding(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates a commercial building based on the specified type.
+ * 
+ * This method is not implemented for landmark factories.
+ * 
+ * @param type A string representing the type of commercial building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Commercial* FactLandmarks::createComBuilding(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates an industrial building based on the specified type.
+ * 
+ * This method is not implemented for landmark factories.
+ * 
+ * @param type A string representing the type of industrial building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Industrial* FactLandmarks::createIndBuilding(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates a service building based on the specified type.
+ * 
+ * This method is not implemented for landmark factories.
+ * 
+ * @param type A string representing the type of service building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Services* FactLandmarks::createServiceBuilding(string type) {
     return nullptr;
 }
-
