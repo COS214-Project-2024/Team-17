cd61f0ecec259484ff48adbd2433bf10af27e353
NicholasDobson <u23671964@tuks.co.za>
2024-11-04 10:59:23 +0200

    Building Doxygen

--- src/Buildings/BuildDecorEconomic.h
+++ src/Buildings/BuildDecorEconomic.h
@@ -3,10 +3,28 @@
 
 #include "BuildingDecorator.h"
 
+/**
+ * @class BuildDecorEconomic
+ * @brief Concrete decorator class that adds economic features to a Building.
+ *
+ * This class extends the BuildingDecorator class and can be used to
+ * apply economic enhancements to a building, potentially affecting its 
+ * financial aspects and display information.
+ */
 class BuildDecorEconomic : public BuildingDecorator {
 public:
+    /**
+     * @brief Constructs a BuildDecorEconomic object that decorates a building.
+     * @param b Pointer to the Building object to be decorated.
+     */
     BuildDecorEconomic(Building* b);
+
+    /**
+     * @brief Displays information about the building with economic enhancements.
+     * This method overrides the displayBuildingInfo method of the 
+     * BuildingDecorator class to include additional economic details.
+     */
     virtual void displayBuildingInfo() override;
 };
 
-#endif
+#endif // BUILDDECORECONOMIC_H
