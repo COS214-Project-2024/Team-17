1beb3327f8f45e1654c047bcd8e25f839d5a73a9
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:54:25 +0200

    Update ResTownhouse.cpp

--- Buildings/ResTownhouse.cpp
+++ Buildings/ResTownhouse.cpp
@@ -1,17 +1,45 @@
 #include "ResTownhouse.h"
 
+/**
+ * @class ResTownhouse
+ * @brief Represents a residential townhouse building.
+ * 
+ * This class provides methods to manage and display information about 
+ * a residential townhouse, including its capacity for people.
+ */
+
+/**
+ * @brief Constructor for the ResTownhouse class.
+ * 
+ * Initializes a new instance of the ResTownhouse and outputs a creation message.
+ */
 ResTownhouse::ResTownhouse() {
     cout << BLACK << "\t-->Townhouse created" << RESET << endl;
 }
 
+/**
+ * @brief Displays information about the townhouse.
+ * 
+ * This method outputs the capacity of the townhouse for accommodating people to the console.
+ */
 void ResTownhouse::displayBuildingInfo() {
     cout << "Townhouse for " << this->capacity << " people\n";
 }
 
+/**
+ * @brief Gets the capacity of the townhouse.
+ * 
+ * @return The current capacity of the townhouse for people.
+ */
 int ResTownhouse::getCapacity() const {
     return capacity;
 }
 
+/**
+ * @brief Sets the capacity of the townhouse.
+ * 
+ * @param capacity The new capacity to set for the townhouse.
+ */
 void ResTownhouse::setCapacity(int capacity) {
     this->capacity = capacity;
 }
