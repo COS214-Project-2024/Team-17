01d79b516753290ca7991a65a13961c5032701eb
NicholasDobson <u23671964@tuks.co.za>
2024-11-04 09:54:28 +0200

    .

--- src/Government+Tax/InfrastructureBudgetPolicy.h
+++ src/Government+Tax/InfrastructureBudgetPolicy.h
@@ -1,3 +1,14 @@
+/**
+ * @file InfrastructureBudgetPolicy.h
+ * @brief Header file for the InfrastructureBudgetPolicy class, which allocates funds for infrastructure projects.
+ * @details This file defines the InfrastructureBudgetPolicy class, a budget policy focused on allocating
+ * funds specifically for infrastructure development within a city structure. It implements the BudgetPolicy
+ * interface to apply infrastructure-centered budgets to city departments.
+ * @see BudgetPolicy for the base policy interface this class implements.
+ * @see CityStructure for the structure this policy interacts with to apply budgets.
+ * @note This class is used in conjunction with the Government class to enforce infrastructure-based budget allocation.
+ * @author WORKONMYMACHINE TEAM
+ */
 #ifndef INFRASTRUCTUREBUDGETPOLICY_H
 #define INFRASTRUCTUREBUDGETPOLICY_H
 
