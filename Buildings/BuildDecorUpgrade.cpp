05062bfc894c5a89c42fc6337db0a16c666c20cc
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:24:20 +0200

    Updated BuildDecorUpgrade

--- Buildings/BuildDecorUpgrade.cpp
+++ Buildings/BuildDecorUpgrade.cpp
@@ -1,7 +1,26 @@
 #include "BuildDecorUpgrade.h"
 
+/**
+ * @class BuildDecorUpgrade
+ * @brief Decorator class that adds decorative upgrades to a building.
+ * 
+ * This class extends BuildingDecorator to add functionality indicating that 
+ * a building has been upgraded with decorative elements.
+ */
+
+/**
+ * @brief Constructor for the BuildDecorUpgrade decorator.
+ * 
+ * @param b Pointer to a Building object to be decorated with decorative elements.
+ */
 BuildDecorUpgrade::BuildDecorUpgrade(Building* b) : BuildingDecorator(b) {}
 
+/**
+ * @brief Displays information about the building, indicating decorative upgrades.
+ * 
+ * If the wrapped building exists, it first calls the base building's 
+ * display method, then adds information about the decorative upgrade.
+ */
 void BuildDecorUpgrade::displayBuildingInfo() {
     if (building) {
         building->displayBuildingInfo();
