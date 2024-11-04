6c22690cc427df0f0e9144a39a9bb78cfcb1b859
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:35:30 +0200

    Update Commercial.cpp

--- Buildings/Commercial.cpp
+++ Buildings/Commercial.cpp
@@ -1,6 +1,21 @@
 #include "Commercial.h"
 
+/**
+ * @class Commercial
+ * @brief Abstract base class for commercial buildings.
+ * 
+ * This class defines the interface for commercial buildings and their operations.
+ */
+
+/**
+ * @brief Performs the main operation for the commercial building.
+ * 
+ * This method is intended to be implemented by derived classes to define specific 
+ * operations for different types of commercial buildings.
+ * 
+ * @throws const char* Indicates that the operation is not yet implemented.
+ */
 void Commercial::operation() {
-	// TODO - implement Commercial::operation
-	throw "Not yet implemented";
+    // TODO - implement Commercial::operation
+    throw "Not yet implemented";
 }
