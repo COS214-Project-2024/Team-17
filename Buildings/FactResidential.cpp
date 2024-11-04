a59f44b4372cf6924eb91a9f7fe28287931df8c2
diyjadwat <diyaanajadwat1@gmail.com>
2024-11-04 13:41:42 +0200

    Update FactResidential.cpp

--- Buildings/FactResidential.cpp
+++ Buildings/FactResidential.cpp
@@ -1,9 +1,29 @@
 #include "FactResidential.h"
 
+/**
+ * @class FactResidential
+ * @brief Factory class for creating residential building instances.
+ * 
+ * This class implements a factory pattern to create instances of 
+ * various residential building types such as houses, townhouses, flats, and estates.
+ */
+
+/**
+ * @brief Constructor for the FactResidential class.
+ * 
+ * Initializes a new instance of the FactResidential and outputs a creation message.
+ */
 FactResidential::FactResidential() {
     cout << BLACK << "\t->Residential factory created" << RESET << endl;
 }
 
+/**
+ * @brief Creates a residential building based on the specified type.
+ * 
+ * @param type A string representing the type of residential building to create.
+ * 
+ * @return A pointer to the created Residential object, or nullptr if the type is not recognized.
+ */
 Residential* FactResidential::createResBuilding(string type) {
     if (type == "House") {
         return new ResHouse();
@@ -17,15 +37,54 @@ Residential* FactResidential::createResBuilding(string type) {
     return nullptr;
 }
 
+/**
+ * @brief Creates a commercial building based on the specified type.
+ * 
+ * This method is not implemented for residential factories.
+ * 
+ * @param type A string representing the type of commercial building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Commercial* FactResidential::createComBuilding(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates an industrial building based on the specified type.
+ * 
+ * This method is not implemented for residential factories.
+ * 
+ * @param type A string representing the type of industrial building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Industrial* FactResidential::createIndBuilding(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates a landmark based on the specified type.
+ * 
+ * This method is not implemented for residential factories.
+ * 
+ * @param type A string representing the type of landmark to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Landmark* FactResidential::createLandmark(string type) {
     return nullptr;
 }
+
+/**
+ * @brief Creates a service building based on the specified type.
+ * 
+ * This method is not implemented for residential factories.
+ * 
+ * @param type A string representing the type of service building to create.
+ * 
+ * @return Always returns nullptr.
+ */
 Services* FactResidential::createServiceBuilding(string type) {
     return nullptr;
 }
