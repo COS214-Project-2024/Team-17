5c09235529fbbc0a05c44d42fe08c93b546c6b5f
NicholasDobson <u23671964@tuks.co.za>
2024-11-04 09:33:52 +0200

    .

--- src/Government+Tax/CollectTaxCommand.cpp
+++ src/Government+Tax/CollectTaxCommand.cpp
@@ -1,11 +1,17 @@
 #include "CollectTaxCommand.h"
 #include <iostream>
-
+/**
+ * @brief Construct a new CollectTaxCommand object.
+ * 
+ * @param gov Pointer to the Government instance.
+ */
 CollectTaxCommand::CollectTaxCommand(Government *gov)
 {
   this->government = gov;
 }
-
+/**
+ * @brief Execute the command to collect taxes for the government.
+ */
 void CollectTaxCommand::execute()
 {
   this->government->collectTaxes();
